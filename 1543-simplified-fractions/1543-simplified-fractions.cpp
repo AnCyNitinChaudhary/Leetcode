a19187f5d5f95d2d58639739f38dd2a9029df724
Nitin Chaudhary <109977558+AnCyNitinChaudhary@users.noreply.github.com>
2023-08-11 19:50:15 +0530

    Time: 52 ms (89.2%), Space: 21.9 MB (53.27%) - LeetHub

--- 1543-simplified-fractions/1543-simplified-fractions.cpp
+++ 1543-simplified-fractions/1543-simplified-fractions.cpp
@@ -1,15 +1,32 @@
 class Solution {
 public:
-    vector<string> simplifiedFractions(int n) {
+//First appraoch using unordered_set and Brute force
+    // vector<string> simplifiedFractions(int n) {
+    //     vector<string> ans;
+    //     unordered_set<double>us;
+    //     for(double i=1;i<=n-1;i++){
+    //         for(double j=2;j<=n;j++){
+    //             double temp=i/j;
+    //             if(temp!=(int)temp&&us.find(temp)==us.end()&&temp<1.0){
+    //                 string str=to_string((int)i)+'/'+to_string((int)j);
+    //                 ans.push_back(str);
+    //                 us.insert(temp);
+    //             }
+    //         }
+    //     }
+    //     return ans;
+    // }
+
+
+    //Second appraoch
+        vector<string> simplifiedFractions(int n) {
         vector<string> ans;
-        unordered_set<double>us;
-        for(double i=1;i<=n-1;i++){
-            for(double j=2;j<=n;j++){
-                double temp=i/j;
-                if(temp!=(int)temp&&us.find(temp)==us.end()&&temp<1.0){
+            for(int j=2;j<=n;j++){
+                for(int i=1;i<j;i++){
+                    int temp=i/j;
+                if(__gcd(i,j)==1){
                     string str=to_string((int)i)+'/'+to_string((int)j);
                     ans.push_back(str);
-                    us.insert(temp);
                 }
             }
         }
