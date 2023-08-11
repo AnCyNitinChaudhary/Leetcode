241ce395283d36d38b21bef10c0461d992709b9a
Nitin Chaudhary <109977558+AnCyNitinChaudhary@users.noreply.github.com>
2023-08-11 17:56:51 +0530

    Time: 0 ms (100%), Space: 6 MB (8.98%) - LeetHub

--- 2491-smallest-even-multiple/2491-smallest-even-multiple.cpp
+++ 2491-smallest-even-multiple/2491-smallest-even-multiple.cpp
@@ -1,8 +1,18 @@
+//First Appraoch
+// class Solution {
+// public:
+//     int smallestEvenMultiple(int n) {
+//         int g=__gcd(2,n);
+//         int ans=(2*n)/g;
+//         return ans;
+//     }
+// };
+
+//Second Approach
 class Solution {
 public:
     int smallestEvenMultiple(int n) {
-        int g=__gcd(2,n);
-        int ans=(2*n)/g;
-        return ans;
+        if(n%2==0)return n;
+        return n*2;
     }
 };
