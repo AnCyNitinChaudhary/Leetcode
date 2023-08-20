dc0a577617651dc4f878b81fabc4237138383115
Nitin Chaudhary <109977558+AnCyNitinChaudhary@users.noreply.github.com>
2023-08-20 09:36:18 +0530

    Time: 261 ms (100%), Space: 179 MB (100%) - LeetHub

---
+++ 2832-find-the-longest-equal-subarray/2832-find-the-longest-equal-subarray.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    // int f(vector<int>&nums,int k,int last,int deleted,int i,int len){
+    //     if(i>=nums.size())return len;
+    //     if(deleted==k)return len;
+    //     int take=0;
+    //     if(last==-1||nums[i]==last){
+    //         take=f(nums,k,last,deleted,i+1,len+1);
+    //     }
+    //     int notTake=0;
+    //     if(last==-1)notTake=f(nums,k,last,deleted,i+1,len);
+    //     else notTake=f(nums,k,nums[i],deleted+1,i+1,len);
+    //     return max(take,notTake);
+    // }
+    int longestEqualSubarray(vector<int>& nums, int k){
+        int left=0;
+        unordered_map<int,int>freqMap;
+        int maxi=0;
+        for (int right=0;right<nums.size();++right){
+            freqMap[nums[right]]++;
+            maxi=max(maxi,freqMap[nums[right]]);
+            int deletionsNeeded =(right-left+1)-maxi;
+            
+            while (deletionsNeeded>k){
+                freqMap[nums[left]]--;
+                left++;
+                deletionsNeeded=(right-left+1)-maxi; 
+            }
+        } 
+        return maxi;
+    }
+//     int longestEqualSubarray(vector<int>& nums, int k) {
+//         //Try out all possible ways
+//         // return f(nums,k,-1,0,0,0);
+        
+//         //sliding window approach
+        
+//     }
+};
