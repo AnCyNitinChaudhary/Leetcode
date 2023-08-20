bf4012123c8b6a5233ef413d0e49af654c2ee1db
Nitin Chaudhary <109977558+AnCyNitinChaudhary@users.noreply.github.com>
2023-08-20 19:59:48 +0530

    Time: 25 ms (75.99%), Space: 6.4 MB (30.75%) - LeetHub

---
+++ 0037-sudoku-solver/0037-sudoku-solver.cpp
@@ -0,0 +1,81 @@
+class Solution {
+public:
+    // bool isValid(vector<vector<char>>&board,int row,int col,char c){
+    //     for(int i=0;i<9;i++){
+    //         if(board[i][col]==c)return false;
+    //         if(board[row][i]==c)return false;
+    //     }
+        // int startrow=3*(row/3);
+        // int startcol=3*(col/3);
+        // for(int i=startrow;i<startrow+3;i++){
+        //     for(int j=startcol;j<startcol+3;j++){
+        //         if(board[i][j]==c)return false;
+        //     }
+        // }
+        // return true;
+    // }
+    // bool solve(vector<vector<char>>&board){
+    //         for(int i=0;i<board.size();i++){
+    //         for(int j=0;j<board[0].size();j++){
+    //             if(board[i][j]=='.'){
+    //                 for(char c='1';c<='9';c++){
+    //                     if(isValid(board,i,j,c)){
+    //                         board[i][j]=c;
+    //                         if(solve(board))return true;
+    //                         else board[i][j]='.';
+    //                     }
+    //                 }
+    //                 return false;
+    //             }
+    //         }
+    //     }
+    //     return true;
+    // }
+
+
+
+
+
+
+
+
+
+    //Solving for intuit preparation-
+    bool canplace(char c,vector<vector<char>>&board,int row,int col){
+        for(int i=0;i<9;i++){
+            if(board[row][i]==c)return false;
+            else if(board[i][col]==c)return false;
+            //the third case is difficult for checking 3X3, so understanding from the striver but will sovle again.
+        }
+        int startrow=3*(row/3);
+        int startcol=3*(col/3);
+        for(int i=startrow;i<startrow+3;i++){
+            for(int j=startcol;j<startcol+3;j++){
+                if(board[i][j]==c)return false;
+            }
+        }
+        return true;
+    }
+    bool solve(vector<vector<char>>&board){
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+                if(board[i][j]=='.'){
+                        for(char c='1';c<='9';c++){
+                            if(canplace(c,board,i,j)){
+                                board[i][j]=c;
+                                if(solve(board))return true;
+                                else board[i][j]='.';
+                            }
+                        }
+                        return false;
+                }
+            }
+        }
+        return true;
+    }
+    void solveSudoku(vector<vector<char>>& board) {
+        // solve(board);
+        bool temp=solve(board);
+    }
+
+};
