2077cd28b1a5110754459961c2cf73208ea9fe2e
Nitin Chaudhary <109977558+AnCyNitinChaudhary@users.noreply.github.com>
2023-08-26 13:25:34 +0530

    Time: 0 ms (100%), Space: 7 MB (93.19%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,23 +1,46 @@
 class Solution {
 public:
-    void f(int i,vector<int>&nums,vector<int>&temp,set<vector<int>>&us){
-        if(i<0){
-            us.insert(temp);
-            return;
-        }
-        //take
-        temp.push_back(nums[i]);
-        f(i-1,nums,temp,us);
-        temp.pop_back();
-        //not Take
-        f(i-1,nums,temp,us);
-    }
+    // void f(int i,vector<int>&nums,vector<int>&temp,set<vector<int>>&us){
+    //     if(i<0){
+    //         us.insert(temp);
+    //         return;
+    //     }
+    //     //take
+    //     temp.push_back(nums[i]);
+    //     f(i-1,nums,temp,us);
+    //     temp.pop_back();
+    //     //not Take
+    //     f(i-1,nums,temp,us);
+    // }
     vector<vector<int>> subsets(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        set<vector<int>>us;
-        vector<int>temp;
-        f(nums.size()-1,nums,temp,us);
-        vector<vector<int>>ans(us.begin(),us.end());
-        return ans;
+        // sort(nums.begin(),nums.end());
+        // set<vector<int>>us;
+        // vector<int>temp;
+        // f(nums.size()-1,nums,temp,us);
+        // vector<vector<int>>ans(us.begin(),us.end());
+        // return ans;
+        //using the powerset method of bit manipulation as taught by striver.
+        // int n=nums.size();
+        // int possibilities=pow(2,n)-1;
+        // set<vector<int>>us;
+        // for(int i=0;i<=possibilities;i++){
+        //     vector<int>temp;
+        //     for(int j=0;j<n;j++){
+        //         if(i&&(1<<j))temp.push_back(nums[j]);
+        //     }
+        //     us.insert(temp);
+        // }
+        // vector<vector<int>>ans(us.begin(),us.end());
+        // return ans;
+        int n = nums.size(), p = 1 << n;
+        vector<vector<int>> subs(p);
+        for (int i = 0; i < p; i++) {
+            for (int j = 0; j < n; j++) {
+                if ((i >> j) & 1) {
+                    subs[i].push_back(nums[j]);
+                }
+            }
+        }
+        return subs;
     }
 };
